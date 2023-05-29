3161df1967f09fdb3060e06076858812054feedf
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-05-29 22:04:31 +0530

    add sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import React from 'react';
+import { BiMenuAltLeft } from "react-icons/bi";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  HStack,
+  VStack,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <div>Header</div>
+    <>
+      <Button
+        pos={"fixed"}
+        top={'2'}
+        left={'2'}
+        colorScheme='purple'
+        p={'0'}
+        w={'10'}
+        h={'10'}
+        borderRadius={'full'}
+        onClick={onOpen}
+      >
+        <BiMenuAltLeft size={'20'} />
+      </Button>
+      <Drawer isOpen={isOpen} placement='left' onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>
+            VIDEO HUB
+          </DrawerHeader>
+          <DrawerBody>
+            <VStack alignItems={'flex-start'}>
+              <Button onClick={onClose} variant={'ghost'} colorScheme='purple'>
+                <Link to={'/'}>Home</Link>
+              </Button>
+              <Button onClick={onClose} variant={'ghost'} colorScheme='purple'>
+                <Link to={'/videos'}>Videos</Link>
+              </Button>
+              <Button onClick={onClose} variant={'ghost'} colorScheme='purple'>
+                <Link to={'/videos?category=free'}>Free Videos</Link>
+              </Button>
+              <Button onClick={onClose} variant={'ghost'} colorScheme='purple'>
+                <Link to={'/upload'}>Upload Video</Link>
+              </Button>
+            </VStack>
+            <HStack
+              pos={'absolute'}
+              bottom={'10'}
+              left={'0'}
+              w={'full'}
+              justifyContent={'space-evenly'}
+            >
+              <Button onClick={onClose} colorScheme='purple'>
+                <Link to={'/login'}>Log In</Link>
+              </Button>
+              <Button onClick={onClose} colorScheme='purple' variant={'outline'}>
+                <Link to={'signup'}>Sign Up</Link>
+              </Button>
+            </HStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
   )
 }
 
