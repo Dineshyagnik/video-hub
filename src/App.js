4a3bd5506933d367c030c59c66aff9b5324ba1ed
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-06-02 23:29:13 +0530

    upload video added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { BrowserRouter as Router,Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Videos from "./components/Videos";
+import Upload from "./components/Upload";
 
 
 function App() {
   return (
     <Router>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/videos" element={<Videos/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/videos" element={<Videos />} />
+        <Route path="/upload" element={<Upload />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
